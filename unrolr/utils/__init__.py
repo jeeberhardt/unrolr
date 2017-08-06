0a8036c763f36ddfb9fc33e718498b89cb5a292a
jeeeberhardt <qksoneo@gmail.com>
2017-08-06 17:27:46 +0200

    add read/save dataset (HDF5) function

---
+++ unrolr/utils/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Jérôme Eberhardt 2016-2017
+# Unrolr
+# Author: Jérôme Eberhardt <qksonoe@gmail.com>
+#
+# License: MIT
+
+from .dataset import read_dataset
+from .dataset import save_dataset
+
+__all__ = ["read_dataset", "save_dataset"]
---
+++ unrolr/utils/dataset.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Jérôme Eberhardt 2016-2017
+# Unrolr
+#
+# Function to read and save data from/to a HDF5 file
+# Author: Jérôme Eberhardt <qksonoe@gmail.com>
+#
+# License: MIT
+
+import h5py
+import numpy as np
+
+__author__ = "Jérôme Eberhardt"
+__copyright__ = "Copyright 2017, Jérôme Eberhardt"
+
+__lience__ = "MIT"
+__maintainer__ = "Jérôme Eberhardt"
+__email__ = "qksoneo@gmail.com"
+
+
+def read_dataset(fname, dname, start=0, stop=-1, skip=1):
+
+    data = None
+
+    with h5py.File(fname, 'r') as f:
+        try:
+            if stop == -1:
+                data[:] = f[dname][start::skip,]
+            else:
+                data[:] = f[dname][start:stop:skip,]
+        except:
+            pass
+
+    return data
+
+def save_dataset(fname, dname, data):
+    with h5py.File(fname, 'w') as w:
+        try:
+            dset = w.create_dataset(dname, (data.shape[0], data.shape[1]))
+            dset[:] = data
+        except:
+            pass
+
+        w.flush()
