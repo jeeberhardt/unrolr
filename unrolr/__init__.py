d69398bb5ac61e505d9a7b08676c341f5d07a7d5
jeeeberhardt <qksoneo@gmail.com>
2017-08-07 15:27:22 +0200

    add Unrolr in the python path

---
+++ unrolr/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Jérôme Eberhardt 2016-2017
+# Unrolr
+# Author: Jérôme Eberhardt <qksonoe@gmail.com>
+#
+# License: MIT
+
+
+from .unrolr import Unrolr
+
+__all__ = ["Unrolr"]
