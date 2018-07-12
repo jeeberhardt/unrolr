358158e331414e20870766c8da4a4a320ee779d0
Jérôme Eberhardt <qksoneo@gmail.com>
2018-07-11 18:31:39 -0700

    can extract intramolecular distance

---
+++ unrolr/feature_extraction/intramolecular_distances.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Jérôme Eberhardt 2016-2018
+# Unrolr
+#
+# Extract intramolecular distances from trajectories
+# Author: Jérôme Eberhardt <qksonoe@gmail.com>
+#
+# License: MIT
+
+
+import argparse
+
+import h5py
+import numpy as np
+from MDAnalysis import Universe
+from MDAnalysis.analysis.base import AnalysisBase
+from MDAnalysis.analysis.distances import self_distance_array
+
+from ..utils import save_dataset
+
+__author__ = "Jérôme Eberhardt"
+__copyright__ = "Copyright 2018, Jérôme Eberhardt"
+
+__lience__ = "MIT"
+__maintainer__ = "Jérôme Eberhardt"
+__email__ = "qksoneo@gmail.com"
+
+
+class IntramolecularDistance(AnalysisBase):
+    def __init__(self, top_file, trj_files, selection='backbone', **kwargs):
+        # Used to store the result
+        self.result = []
+
+        self._u = Universe(top_file, trj_files)
+        self._ag = self._u.select_atoms(selection)
+        super(IntramolecularDistance, self).__init__(self._ag.universe.trajectory, **kwargs)
+
+    def _single_frame(self):
+        self.result.append(self_distance_array(self._ag.positions))
+
+    def _conclude(self):
+        self.result = np.asarray(self.result)
+
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Extract intramolecular distances")
+    parser.add_argument("-p", "--top", dest="top_file", required=True,
+                        action="store", type=str,
+                        help="topology file used for simulation (pdb, psf)")
+    parser.add_argument("-t", "--trj", dest="trj_files", required=True,
+                        action="store", type=str, nargs="+",
+                        help="list of trj files")
+    parser.add_argument("-s", "--selection", dest="selection",
+                        action="store", type=str,
+                        default="backbone", help="residu selection")
+    parser.add_argument("-o" "--output", dest="output",
+                        action="store", type=str, default="intramolecular_distances.h5",
+                        help="directory output")
+    options = parser.parse_args()
+
+    top_file = options.top_file
+    trj_files = options.trj_files
+    selection = options.selection
+    output = options.output
+
+    i = IntramolecularDistance(top_file, trj_files, selection).run()
+    data = i.result
+
+    save_dataset(output, "intramolecular_distances", data)
+
+if __name__ == '__main__':
+    main()
