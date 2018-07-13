0bd2f42a4df3b166c8a8ffbe97e6ba440edb1f92
Jérôme Eberhardt <qksoneo@gmail.com>
2018-07-13 09:47:55 -0700

    use path.join instead of concatenation

--- unrolr/core/unrolr.py
+++ unrolr/core/unrolr.py
@@ -59,7 +59,7 @@ class Unrolr():
 
         # Read OpenCL kernel file
         path = imp.find_module('unrolr')[1]
-        fname = path + '/core/kernel.cl'
+        fname = os.path.join(path, '/core/kernel.cl')
 
         with open(fname) as f:
             self._kernel = f.read()
